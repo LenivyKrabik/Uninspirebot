7dbeb3ae59077fba11e1bd11d21a41fff9fc2a99
Lenivy_krabik <chuvachok642@gmail.com>
2026-03-11 17:34:19 +0200

    Removed App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./App.tsx";
+import Header from "./elements/header";
+import Body from "./elements/body";
 
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Layout />
   </StrictMode>,
 );
