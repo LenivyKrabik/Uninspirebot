34515bb2d602ad9fbc2d55b0d8257f243f7b566e
Lenivy_krabik <chuvachok642@gmail.com>
2026-03-11 17:48:37 +0200

    Added header

--- frontend/src/elements/header.tsx
+++ frontend/src/elements/header.tsx
@@ -1,7 +1,11 @@
 import "../styles/header.css";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="homeButton">Uninspirebot</div>
+    </div>
+  );
 }
 
 export default Header;
