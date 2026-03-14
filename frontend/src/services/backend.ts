55b5a59e3b01ff1a37dd698f2550055d8fb519e8
Lenivy_krabik <chuvachok642@gmail.com>
2026-03-15 00:03:25 +0200

    Added test text and text timed audio pathes

--- frontend/src/services/backend.ts
+++ frontend/src/services/backend.ts
@@ -1,5 +1,14 @@
 const backendURL = "http://localhost:3000/";
 
+async function getTestTextWisdom() {
+  const answer = await fetch(backendURL + "testTextWisdom");
+  if (!answer.ok) throw new Error("Can't connect to backend");
+  else {
+    const wisdom = await answer.text();
+    return wisdom;
+  }
+}
+
 async function getTextWisdom() {
   const answer = await fetch(backendURL + "textWisdom");
   if (!answer.ok) throw new Error("Can't connect to backend");
@@ -9,4 +18,21 @@ async function getTextWisdom() {
   }
 }
 
-export default { getTextWisdom: getTextWisdom };
+async function getTextTimedAudioWisdom(): Promise<{
+  audio: any;
+  alignment: any;
+  text: string;
+}> {
+  const answer = await fetch(backendURL + "textTimedAudio");
+  if (!answer.ok) throw new Error("Can't connect to backend");
+  else {
+    const wisdom = await answer.json();
+    return wisdom;
+  }
+}
+
+export default {
+  getTestTextWisdom: getTestTextWisdom,
+  getTextWisdom: getTextWisdom,
+  getTextTimedAudioWisdom: getTextTimedAudioWisdom,
+};
