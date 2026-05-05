edcde0afcd340899e47a4d18d802309f3502d0b1
Lenivy_krabik <chuvachok642@gmail.com>
2026-05-05 16:47:12 +0300

    Added size limit

--- frontend/src/services/simpleQueue.ts
+++ frontend/src/services/simpleQueue.ts
@@ -1,7 +1,13 @@
 class queue<T> {
   mainStorage: T[] = [];
+  maxSize: number;
+
+  constructor(size: number = Infinity) {
+    this.maxSize = size;
+  }
 
   enqueue(item: T) {
+    if (this.mainStorage.length >= this.maxSize) throw new Error("Queue overflow");
     this.mainStorage.push(item);
   }
   dequeue() {
@@ -9,6 +15,9 @@ class queue<T> {
   }
   peek() {
     return this.mainStorage[0];
+  }
+  size() {
+    return this.mainStorage.length;
   }
 }
 
