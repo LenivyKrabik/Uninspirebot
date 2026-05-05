5f522968070fa00b29e8e07d90d04ca1fe12d87c
Lenivy_krabik <chuvachok642@gmail.com>
2026-05-05 17:12:48 +0300

    Added wisdom queue and stable wait time in between wisdoms, also delay for cleaner code

--- frontend/src/elements/lockInScreen.tsx
+++ frontend/src/elements/lockInScreen.tsx
@@ -3,12 +3,21 @@ import "../styles/lockInPage.css";
 import useAudio from "./useAudio";
 import EventEmitter from "../services/eventEmitter";
 import ServicesContext from "../services/servicesContext";
+import queue from "../services/simpleQueue";
+
+import type { timedAudioResponse } from "../services/backend";
 
 const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+const delay = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 function LockInScreen() {
+  const wisdomsQueueSize = 5;
+
   const [lockedIn, setLockedIn] = useState(false);
   const [buttonClass, setButtonClass] = useState("enterLockInButton");
   const [textPosition, setTextPosition] = useState({ top: 0, left: 0 });
@@ -17,13 +26,36 @@ function LockInScreen() {
   const [playingWisdomAudio, toggleWisdomAudio, changeWisdomAudio] = useAudio("");
   const [playingEffectAudio, toggleEffectAudio, changeEffectAudio] = useAudio("");
   const eventManager = useRef(new EventEmitter());
+  const wisdomsQueue = useRef(new queue<timedAudioResponse>(wisdomsQueueSize));
+  const fillingUpQueue = useRef(false);
 
   const { backend } = useContext(ServicesContext);
 
   const audioSettings = "data:audio/mpeg;base64,";
 
+  const fillWisdomsQueue = () => {
+    console.log("Filling up wisdoms queue");
+    fillingUpQueue.current = true;
+    const amountToFill = wisdomsQueueSize - wisdomsQueue.current.size();
+    let doneWith = 0;
+    for (let i = 0; i < amountToFill; i++) {
+      backend
+        .getTextTimedAudioWisdom()
+        .then((value) => {
+          wisdomsQueue.current.enqueue(value);
+        })
+        .catch((error) => {
+          console.warn(`Couldn't get wisdom: ${error}`);
+        })
+        .finally(() => {
+          if (++doneWith == amountToFill) fillingUpQueue.current = false;
+        });
+    }
+  };
+
   //Starting LockIn sesion
   const enterLockedInState = () => {
+    fillWisdomsQueue();
     setButtonClass(buttonClass + " fading");
     setTimeout(() => {
       setLockedIn(true);
@@ -32,37 +64,49 @@ function LockInScreen() {
 
   const lockInWisdomCycle = async () => {
     if (lockedIn) {
-      const wisdom = await backend.getTextTimedAudioWisdom();
-      if (wisdom === undefined) return; //ToDo: signal that can't connect to backend
-      //await backend.getTestTextWisdom();
-      setTextPosition({ top: getRandomInt(6, 60), left: getRandomInt(6, 50) });
-      changeWisdomAudio(audioSettings + wisdom.audio);
-      toggleWisdomAudio();
-
-      //Adding typewriter effect with custom timings from alignment
-      const textLength = wisdom.text.length;
-      for (let charId = 0; charId <= textLength; charId++) {
-        const char = wisdom.alignment.characters[charId];
-        const charStartTime = wisdom.alignment.character_start_times_seconds[charId];
-
-        setTimeout(() => {
-          setShownWisdomText((c) => {
-            if (char !== undefined) return c + char;
-            return c;
-          });
-        }, charStartTime * 1000);
+      if (wisdomsQueue.current.size() <= 2 && !fillingUpQueue.current) {
+        console.log("Need more wisdoms");
+        fillWisdomsQueue();
       }
+      await delay(2000);
 
-      //Audio effects at the end of wisdom
-      const audioFinishTime = wisdom.alignment.character_end_times_seconds[textLength - 1];
-      setTimeout(
-        () => {
-          eventManager.current.emit("wisdomEnd");
-          setShownWisdomText("");
-          setWisdomCount((c) => c + 1);
-        },
-        (audioFinishTime + 1) * 1000,
-      );
+      if (wisdomsQueue.current.size() == 0) {
+        console.log("Don't have any wisdoms");
+        await delay(500);
+        setWisdomCount((c) => c + 1);
+      } else {
+        const wisdom = wisdomsQueue.current.dequeue();
+        if (wisdom === undefined) return; //ToDo: signal that can't connect to backend
+        //await backend.getTestTextWisdom();
+        setTextPosition({ top: getRandomInt(6, 60), left: getRandomInt(6, 50) });
+        changeWisdomAudio(audioSettings + wisdom.audio);
+        toggleWisdomAudio();
+
+        //Adding typewriter effect with custom timings from alignment
+        const textLength = wisdom.text.length;
+        for (let charId = 0; charId <= textLength; charId++) {
+          const char = wisdom.alignment.characters[charId];
+          const charStartTime = wisdom.alignment.character_start_times_seconds[charId];
+
+          setTimeout(() => {
+            setShownWisdomText((c) => {
+              if (char !== undefined) return c + char;
+              return c;
+            });
+          }, charStartTime * 1000);
+        }
+
+        //Audio effects at the end of wisdom
+        const audioFinishTime = wisdom.alignment.character_end_times_seconds[textLength - 1];
+        setTimeout(
+          () => {
+            eventManager.current.emit("wisdomEnd");
+            setShownWisdomText("");
+            setWisdomCount((c) => c + 1);
+          },
+          (audioFinishTime + 1) * 1000,
+        );
+      }
     }
   };
   //Play sound effect
